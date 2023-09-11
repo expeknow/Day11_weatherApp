0890de42389df92662635caea6869b468cc34809
expeknow <atul.k.study@gmail.com>
2023-09-12 02:47:53 +0530

    Day output

---
+++ app/src/main/java/com/example/day11_weatherapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.day11_weatherapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+////https://dribbble.com/shots/11032680-Weather
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
